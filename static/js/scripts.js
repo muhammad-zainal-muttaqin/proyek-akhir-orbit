1b23317dab79e8592c49d9316a54c5424ae9ca8f
Muhammad Zainal Muttaqin <2111016210009@mhs.ulm.ac.id>
2023-12-05 16:34:04 +0800

    Final

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded",function(){var mybutton=document.getElementById("backToTop");window.onscroll=function(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){mybutton.style.display="block";}else{mybutton.style.display="none";}};mybutton.addEventListener("click",function(){document.body.scrollTop=0;document.documentElement.scrollTop=0;});});
+document.addEventListener("DOMContentLoaded", function () {
+    var mybutton = document.getElementById("backToTop");
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    };
+    mybutton.addEventListener("click", function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+});
